4e2e41ab963f0d24edb43742b8ab71d21107da8f
Simonas <simonas@kazlauskas.me>
2011-08-19 22:53:28 +0300

    Settings

--- django_bfm/settings.py
+++ django_bfm/settings.py
@@ -1,12 +1,8 @@
 from django.conf import settings
 
-#Have to think of better way?
-try:
-    MEDIA_DIRECTORY = settings.BFM_MEDIA_DIRECTORY
-except:
-    MEDIA_DIRECTORY = settings.MEDIA_ROOT
-
-try:
-    FILES_IN_PAGE = settings.BFM_FILES_IN_PAGE
-except:
-    FILES_IN_PAGE = 25
+MEDIA_DIRECTORY = getattr(settings,
+                          "BFM_MEDIA_DIRECTORY",
+                          settings.MEDIA_ROOT)
+FILES_IN_PAGE = getattr(settings,
+                        "BFM_FILES_IN_PAGE",
+                        20)
