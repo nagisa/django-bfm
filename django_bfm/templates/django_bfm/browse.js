bce20ed70c2dbab3999c1c50ec1eacbfd0375e76
Simonas <simonas@kazlauskas.me>
2011-08-25 21:17:45 +0300

    Image resizing/Thumbnailing.

--- django_bfm/templates/django_bfm/browse.js
+++ django_bfm/templates/django_bfm/browse.js
@@ -5,7 +5,7 @@ this.imagePreview = function(files){
         this.t = this.title;
         this.title = "";
         var c = (this.t != "") ? "<br/>" + this.t : "";
-        var link = $(this).find('a').attr('href')
+        var link = this.href
         $("body").append("<p id='preview'><img src='"+ link +"' alt='Image preview' />"+ c +"</p>");
         $("#preview")
             .css("top",(e.pageY - xOffset) + "px")
@@ -22,6 +22,68 @@ this.imagePreview = function(files){
             .css("left",(e.pageX + yOffset) + "px");
     });
 };
+
+this.image_info = function(parent){
+    filename = parent.find('.filename').text()
+    $.getJSON('../__image_info/?d='+directory+'&image='+encodeURIComponent(filename), function(data){
+        image_resize(data, filename)
+    })
+};
+
+function rewrite_file_if_exists(filename){
+    var exist = false,
+    question = 'File with name '+filename+' already exists. Rewrite?';
+    $.ajax({
+        url: '../__exists/?f='+encodeURIComponent(filename)+'&d='+encodeURIComponent(directory),
+        async: false}).success(function(rsp){exist = rsp == "YES"});
+    return exist ? confirm(question) : true;
+}
+
+//Totally needs cleanup
+this.image_resize = function(data, fname){
+    var dialog = $('.image-resize'), form = dialog.find('form');
+    var new_name = fname.split('.'), ext = new_name.pop();
+    new_name = new_name.join('.')+'_resized.'+ext
+    form.find('input[name="new_name"]').val(new_name).keyup(function(){
+        if(this.value != form.find('input[name="old_name"]').val()){
+            form.find('.warning').hide()
+        }else{
+            form.find('.warning').show()
+        }
+    })
+    form.find('input[name="old_name"]').val(fname)
+    form.find('input[name="ratio"]').val(data.size[0]/data.size[1])
+    form.find('input[name="width"]').val(data.size[0]).keyup(function(){
+        if(form.find('input[name="lock"]').prop("checked")){
+            form.find('input[name="height"]').val(~~(this.value/form.find('input[name="ratio"]').val()))
+        }
+    })
+    form.find('input[name="height"]').val(data.size[1]).keyup(function(){
+        if(form.find('input[name="lock"]').prop("checked")){
+            form.find('input[name="width"]').val(~~(this.value*form.find('input[name="ratio"]').val()))
+        }
+    })
+    form.find('input[name="ratio"]').val(data.size[0]/data.size[1])
+    console.log(data.size[0]/data.size[1])
+    form.unbind();
+    form.submit(function(){
+        if(!rewrite_file_if_exists(form.find('input[name="new_name"]').val())){
+            return false;
+        }
+        form.find('.error').hide();
+        $.ajax({'url': '../__resize_image/', 'data': {'image': fname,
+                'd': directory,
+                'nimage':form.find('input[name="new_name"]').val(),
+                'width': form.find('input[name="width"]').val(),
+                'height': form.find('input[name="height"]').val(),
+                'filter': form.find('select[name="filter"] option:selected').val()},
+                'success': function(){dialog.hide()},
+                'error': function(){form.find('.error').text('Unexpected error').show()}
+                })
+        return false;
+    })
+    dialog.show()
+}
 
 function dir_up(){
     var l = window.location, tmp;
@@ -43,10 +105,13 @@ $(function(){
             })
         }
     })
-    var files = $('.file td').has('a[href$=".png"]')
+    var files = $('.file td a[href$=".png"]')
     var exts = ['.jpg', '.jpeg', '.gif', '.svg']
     for(key in exts){
-        files = $.merge(files, $('.file td').has('a[href$="'+exts[key]+'"]'))
+        files = $.merge(files, $('.file td a[href$="'+exts[key]+'"]'))
     }
     imagePreview(files);
+    $('.resize-button').click(function(){
+        image_info($(this).parent().parent().parent())
+    })
 })
--- django_bfm/urls.py
+++ django_bfm/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'Dlog.views.home', name='home'),
     url(r'^$', 'django_bfm.views.redirect'),
     url(r'^browse/$', 'django_bfm.views.browse'),
     url(r'^upload/$', 'django_bfm.views.upload'),
     url(r'^__exists/$', 'django_bfm.views._check_file'),
     url(r'^upload/file/$', 'django_bfm.views._upload_file'),
     url(r'^browse/select/$', 'django_bfm.views.select'),
+    url(r'^__image_info/$', 'django_bfm.im_views.image_info'),
+    url(r'^__resize_image/$', 'django_bfm.im_views.image_resize'),
 )
