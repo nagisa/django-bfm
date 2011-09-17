c2b3bc69ce01e09a61663714b52e919d1cb53dc9
Simonas <simonas@kazlauskas.me>
2011-09-17 20:19:53 +0300

    Base Backbone

--- django_bfm/urls.py
+++ django_bfm/urls.py
@@ -1,12 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^$', 'django_bfm.views.redirect'),
-    url(r'^browse/$', 'django_bfm.views.browse'),
-    url(r'^upload/$', 'django_bfm.views.upload'),
-    url(r'^__exists/$', 'django_bfm.views._check_file'),
-    url(r'^upload/file/$', 'django_bfm.views._upload_file'),
-    url(r'^browse/select/$', 'django_bfm.views.select'),
-    url(r'^__image_info/$', 'django_bfm.im_views.image_info'),
-    url(r'^__resize_image/$', 'django_bfm.im_views.image_resize'),
+    url(r'^$', 'django_bfm.views.base'),
 )
--- django_bfm/views.py
+++ django_bfm/views.py
@@ -1,99 +1,11 @@
 from django.shortcuts import render_to_response
-from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotAllowed, HttpResponseRedirect, Http404
 from django.core.context_processors import csrf
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.admin.views.decorators import staff_member_required
-from django.utils.translation import ugettext as _
-
-import settings
-from forms import UploadFileForm
-from utils import get_dir, Directory
-
-def redirect(request):
-    #Relative path
-    return HttpResponseRedirect('browse/')
 
 @login_required
 @staff_member_required
-def browse(request):
+def base(request):
     c={}
     c.update(csrf(request))
-    directory, rel_dir = get_dir(request.GET)
-    files = directory.list_files()
-    directories = directory.list_directories()
-    pages = Paginator(files, settings.FILES_IN_PAGE)
-    try:
-        page = pages.page(request.GET.get('p', 1))
-    except EmptyPage:
-        raise Http404
-    except PageNotAnInteger:
-        raise Http404
-    c['dirs'] = directories
-    c['current_dir'] = rel_dir
-    c['files'] = page
-    return render_to_response('django_bfm/browse.html', c)
-
-@login_required
-@staff_member_required
-def upload(request):
-    c = {}
-    c.update(csrf(request))
-    form = UploadFileForm()
-    rel_dir = get_dir(request.GET)[1]
-    c['form'] = form
-    c['current_dir'] = rel_dir
-    return render_to_response('django_bfm/upload.html', c)
-
-def _check_file(request):
-    """
-    Check if file already exists on the server.
-    """
-    try:
-        fname = request.GET['f']
-    except KeyError:
-        return HttpResponseBadRequest()
-    directory = get_dir(request.GET)[0]
-    if directory.exists(fname):
-        return HttpResponse('YES')
-    else:
-        return HttpResponse('NO')
-
-@login_required
-@staff_member_required
-def _upload_file(request):
-    """
-    Responsible for saving file
-    """
-    if not request.method == 'POST':
-        return HttpResponseNotAllowed(['POST'])
-    else:
-        directory = get_dir(request.GET)[0]
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            destination = directory.storage.path(request.FILES['file'].name)
-            destination = open(destination, 'wb+')
-            for chunk in request.FILES['file'].chunks():
-                destination.write(chunk)
-            destination.close()
-        return HttpResponse('File received')
-
-@login_required
-@staff_member_required
-def select(request):
-    """
-    Responsible for file deleting and touching
-    """
-    if not request.method == 'POST':
-        return HttpResponseNotAllowed(['POST'])
-    else:
-        directory, rel_dir = get_dir(request.GET)
-        action = request.POST.get('action', False)
-        files = dict(request.POST).get('selected', False)
-        if not files:
-            return HttpResponseRedirect('..')
-        if _('Delete') in _(action):
-            directory.delete_files(files)
-        elif _('Touch') in _(action):
-            directory.touch_files(files)
-        return HttpResponseRedirect('../?d=%s' % rel_dir)
+    return render_to_response('django_bfm/base.html', c)
