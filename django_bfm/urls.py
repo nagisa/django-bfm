ffe24dbdca67a4c6e09be00264f8bd7ec662d1b5
Simonas <simonas@kazlauskas.me>
2011-09-22 19:43:35 +0300

    Going further. Actions are on their way.

--- django_bfm/urls.py
+++ django_bfm/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'django_bfm.views.base'),
-    url(r'^list_files/$', 'django_bfm.views.list_files')
+    url(r'^list_files/$', 'django_bfm.views.list_files'),
+    url(r'^list_directories/$', 'django_bfm.views.list_directories'),
+    url(r'^file/$', 'django_bfm.views.file_actions'),
 )
