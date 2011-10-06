aed33607818e956e606569e487d8d96f9c8fdda8
Simonas <simonas@kazlauskas.me>
2011-10-06 18:01:23 +0300

    Upload image isn't working somehow...

--- django_bfm/__init__.py
+++ django_bfm/__init__.py
@@ -1,5 +1,5 @@
 __version_info__ = ('0', '4')
-__version_label__ = ''
+__version_label__ = '-1'
 __version__ = "%s%s" %('.'.join(__version_info__), __version_label__)
 
 __author__ = 'Simonas Kazlauskas'
