acfbef4cfb442450f7227b1ba354349e09807afe
Simonas <simonas@kazlauskas.me>
2012-01-31 23:13:03 +0200

    Move views to class based generic (oh, really generic?) views. Looks cleaner.

--- django_bfm/urls.py
+++ django_bfm/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls.defaults import patterns, url
+from django_bfm import views
 
 urlpatterns = patterns('',
     url(r'^$', 'django_bfm.views.base', name="bfm_base"),
     url(r'^list_files/$', 'django_bfm.views.list_files'),
     url(r'^list_directories/$', 'django_bfm.views.list_directories'),
-    url(r'^file/$', 'django_bfm.views.file_actions'),
-    url(r'^directory/$', 'django_bfm.views.directory_actions'),
+    url(r'^file/$', views.FileActions.as_view()),
+    url(r'^directory/$', views.DirectoryActions.as_view()),
     url(r'^upfile/$', 'django_bfm.views.file_upload', name="bfm_upload"),
-    url(r'^image/$', 'django_bfm.views.image_actions'),
+    url(r'^image/$', views.ImageActions.as_view()),
     url(r'^admin_options/$', 'django_bfm.views.admin_options', name="bfm_opt")
 )
