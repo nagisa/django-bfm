31590f01308a721dd10850e4e72ca8cc63e40d6d
Simonas <simonas@kazlauskas.me>
2012-02-11 17:08:20 +0200

    Don't use strings in urls.

--- django_bfm/urls.py
+++ django_bfm/urls.py
@@ -2,13 +2,13 @@ from django.conf.urls.defaults import patterns, url
 from django_bfm import views
 
 urlpatterns = patterns('',
-    url(r'^$', 'django_bfm.views.base', name="bfm_base"),
-    url(r'^list_files/$', 'django_bfm.views.list_files'),
-    url(r'^list_directories/$', 'django_bfm.views.list_directories'),
+    url(r'^$', views.base, name="bfm_base"),
+    url(r'^list_files/$', views.list_files),
+    url(r'^list_directories/$', views.list_directories),
     url(r'^file/$', views.FileActions.as_view()),
     url(r'^directory/$', views.DirectoryActions.as_view()),
-    url(r'^upfile/$', 'django_bfm.views.file_upload', name="bfm_upload"),
+    url(r'^upfile/$', views.file_upload, name="bfm_upload"),
     url(r'^image/$', views.ImageActions.as_view()),
-    url(r'^admin_options/$', 'django_bfm.views.admin_options', name="bfm_opt"),
+    url(r'^admin_options/$', views.admin_options, name="bfm_opt"),
     url(r'^client_templates/$', views.client_templates, name='bfm_templates')
 )
